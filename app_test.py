4741cf8d47cc62331c986184e5d4dfd94a515586
Will Palmer <wp624@ic.ac.uk>
2024-10-28 14:16:04 +0000

    fixed styling

--- app_test.py
+++ app_test.py
@@ -11,5 +11,6 @@ def test_knows_about_dinosaurs():
 def test_does_not_know_about_asteroids():
     assert process_query("asteroids") == "Unknown"
 
+
 def test_knows_name():
     assert process_query("What is your name?") == "Will"
